c22de066b3abc2f1667b36c5c3cc2347ab5f65a6
Harrison-Gitau <gitau.h.mwangi@gmail.com>
2019-05-27 16:37:08 +0300

    Import helper libraries

--- mnist.py
+++ mnist.py
@@ -1,1 +1,19 @@
-from __future__ import absolute_imports, division, print_function
+#Classifying images of clothing
+from __future__ import absolute_imports, division, print_function, unicode_literals
+
+#Import Tensorflow and Tensorflow datasets
+import tensorflow as tf
+import tensorflow_datasets as tfds
+tf.logging.set_versobility(tf.logging.ERROR)
+
+#Helper libraries
+import math
+import numpy as np
+import matpotlib.pyplot as plt
+
+#Improve bar progress display
+import tqdm
+import tqdm.auto
+tqdm.tqdm = tqdm.auto.tqdm
+
+print(.tf__version__)
